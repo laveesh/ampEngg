c701189765cb3cb531e9cad12f2babd40c537fe6
Laveesh Gupta <loveeshgoyal@gmail.com>
2019-10-16 21:35:26 +0530

    first final draft

--- src/app/@theme/layouts/one-column/one-column.layout.ts
+++ src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -9,10 +9,6 @@ import { Component } from '@angular/core';
         <ngx-header></ngx-header>
       </nb-layout-header>
 
-      <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive>
-        <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>
-
       <nb-layout-column>
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
